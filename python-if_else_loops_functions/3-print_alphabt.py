af9e83b6a4d23773db35ac9df1a2a9089201f968
StilianSaka <stilisaka@gmail.com>
2024-09-04 12:24:31 +0000

    Print Alphabt

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-print('{}'.format(''.join(chr(i) for i in range(97, 123) if chr(i) not in {'q', 'e'})), end='')
+chars = ''.join(
+    chr(i) for i in range(97, 123) if chr(i) not in {'q', 'e'}
+)
+print('{}'.format(chars), end='')
