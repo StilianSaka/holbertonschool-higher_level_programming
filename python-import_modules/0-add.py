940cd4a29da870ce2f8609b7d4c9d63846234675
StilianSaka <stilisaka@gmail.com>
2024-09-06 10:15:58 +0000

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 from add_0 import add
 
-def main():
-    a = 1
-    b = 2
-    result = add(a, b)
-    print(f"{a} + {b} = {result}")
+a = 1
+b = 2
 
-if __name__ == "__main__":
-    main()
+result = add(a, b)
+print(f"{a} + {b} = {result}")
