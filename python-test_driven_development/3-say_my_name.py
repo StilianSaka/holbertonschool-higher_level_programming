8d3fea2153bf98a6ac73098f2296fc7a4fa1a5ba
StilianSaka <stilisaka@gmail.com>
2024-09-13 11:34:26 +0000

    say my name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# The shebang line specifies the script interpreter to use: Python 3.
 
 def say_my_name(first_name, last_name=""):
     """
@@ -25,8 +24,8 @@ def say_my_name(first_name, last_name=""):
     TypeError: first_name must be a string
     """
     if not isinstance(first_name, str):
-        raise TypeError(f"{first_name} must be a string")
+        raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
-        raise TypeError(f"{last_name} must be a string")
+        raise TypeError("last_name must be a string")
     
     print(f"My name is {first_name} {last_name}")
