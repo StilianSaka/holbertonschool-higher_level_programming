10b9407053e0d395258a2a526662e9a24632f707
StilianSaka <stilisaka@gmail.com>
2024-09-13 10:03:10 +0000

    matrix divided

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -52,5 +52,4 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
     
-
     return [[round(elem / div, 2) for elem in row] for row in matrix]
