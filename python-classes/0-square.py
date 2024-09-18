409730d9407562e5c5156c86f4e1353e8df79791
StilianSaka <stilisaka@gmail.com>
2024-09-18 08:55:58 +0000

    0 square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class.
+
+Classes:
+    Square: An empty class that defines a square.
+"""
+
 
 class Square:
+    """
+    A class used to represent a Square.
 
+    Attributes
+    ----------
+    None
+
+    Methods
+    -------
+    None
+    """
     pass
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class.
+
+Classes:
+    Square: A class that defines a square by its size.
+"""
+
 
 class Square:
+    """
+    A class used to represent a Square.
 
-    pass
+    Attributes
+    ----------
+    __size : int
+        The size of the square (private).
+    """
 
-def __init__(self, size = 0):
-
-    self.size = size
+    def __init__(self, size):
+        """
+        Parameters
+        ----------
+        size : int
+            The size of the square.
+        """
+        self.__size = size
