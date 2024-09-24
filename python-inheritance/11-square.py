526830478cf12f246fee7a66aacb8382bd2a5244
StilianSaka <stilisaka@gmail.com>
2024-09-24 09:38:58 +0000

    square 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Module about a square"""
 
+"""Module about a square"""
 
 Rectangle = __import__('9-rectangle').Rectangle
 
@@ -9,12 +9,12 @@ class Square(Rectangle):
     """Class that implements a square"""
     def __init__(self, size):
         Rectangle.integer_validator(self, "size", size)
-        super().__init__(size, size)
+        Rectangle.__init__(self, size, size)
         self.__size = size
 
     def area(self):
         """Func to calculate area"""
         return self.__size ** 2
-    
+
     def __str__(self):
-        return f"{self.__size}/{self.__size}"
+        return f"[Square] {self.__size}/{self.__size}"
