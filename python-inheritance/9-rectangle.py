606ea54dd1a697bc565e8d82c11911415c10a814
StilianSaka <stilisaka@gmail.com>
2024-09-24 09:20:13 +0000

    9 rectangle

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Module about a rectangle class"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Class about a rectangle"""
+
+    def __init__(self, width, height):
+        BaseGeometry.integer_validator(self, "width", width)
+        BaseGeometry.integer_validator(self, "height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Function that calculates the area of the rectangle"""
+
+        return self.__width * self.__height
+
+        def __str__(self):
+            return f"[Rectangle] {self.__width}/{self.__height}"
