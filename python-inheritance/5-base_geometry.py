d59365e87fc5b38fdc5d56b76d1a8b1d35493b21
StilianSaka <stilisaka@gmail.com>
2024-09-24 08:18:37 +0000

    base geometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module defines the function `lookup` that returns a list
+of available attributes and methods of an object.
+"""
+
+
+class BaseGeometry:
+    """
+    A class used to represent a BaseGeometry.
+
+    Attributes
+    ----------
+    None
+
+    Methods
+    -------
+    None
+    """
+
+    pass
