435032beb41dfe5bcee3aa992f185719a2f769dd
StilianSaka <stilisaka@gmail.com>
2024-10-01 09:22:48 +0000

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    """
-    Reads a text file (UTF8) and prints its contents to stdout.
+    """ Reads a text file (UTF8) and prints its contents to stdout."""
 
-    Args:
-        filename (str): The name of the file to be read.
+    """Args:filename (str): The name of the file to be read."""
 
-    Returns:
-        None
-    """
+    """Returns:None"""
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read())
