7c19dbd12996547c725b8210e6e17f005078cad3
StilianSaka <stilisaka@gmail.com>
2024-10-02 09:32:46 +0000

    student

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 class Student:
-    """A class representing a student with first name, last name, and age."""
+    """Represents a student with first name, last name, and age."""
 
 
     def __init__(self, first_name, last_name, age):
-        """Initalization of the class objects"""
+        """Initialize student attributes."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """Return dictionary representation of the student."""
         return {
             'first_name': self.first_name,
             'last_name': self.last_name,
