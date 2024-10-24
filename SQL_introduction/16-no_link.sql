86134d4c28b22f7d552183c6afef7551f06192e1
StilianSaka <stilisaka@gmail.com>
2024-10-24 08:17:19 +0000

    no link

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,4 +1,6 @@
--- Write a script that computes the average score of all records
+-- Write a script that lists all the records in the second_table
 
-SELECT AVG(score) AS average 
-FROM second_table;
+SELECT score, name 
+FROM second_table 
+WHERE name IS NOT NULL 
+ORDER BY score DESC;
