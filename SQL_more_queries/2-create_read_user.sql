ec776cf71cdcb78ee32d7c1f010ec6732ac406cb
StilianSaka <stilisaka@gmail.com>
2024-10-28 09:54:41 +0000

    create read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -8,5 +8,4 @@ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Write a script that gives the user user_0d_2 read privilege
 
-
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
