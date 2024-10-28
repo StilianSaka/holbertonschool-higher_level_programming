6d51e32d4502c06d7925421a7043ea1e9673c90b
StilianSaka <stilisaka@gmail.com>
2024-10-28 09:48:36 +0000

    create read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,1 +1,7 @@
 -- Write a script that creates the database hbtn_0d_2 and the user user_0d_2
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Write a script that creates the MySQL server user user_0d_2
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
