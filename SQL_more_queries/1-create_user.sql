aff186f4828daa4cb28b0f9d888639156053eeca
StilianSaka <stilisaka@gmail.com>
2024-10-28 09:42:40 +0000

    create user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,7 @@
 -- Write a script that creates the MySQL server user user_0d_1
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- List privileges for user_0d_2
+
 GRANT ALL PRIVILEGES *.* TO 'user_0d_1'@'localhost';
