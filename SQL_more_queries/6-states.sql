8c4c51f6b1425c1d153766b6b2d71649d2ed2195
StilianSaka <stilisaka@gmail.com>
2024-10-28 10:57:05 +0000

    states

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,1 +1,11 @@
--- Write a script that creates the database hbtn_0d_usa and the table states
+-- Write a script that creates the database hbtn_0d_usa and the table states.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Script to create table
+
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT UNIQUE NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (ID)
+);
