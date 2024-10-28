fc879b6ad4f6692a6eccb2c490a7bc266c942353
StilianSaka <stilisaka@gmail.com>
2024-10-28 09:54:00 +0000

    create read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -5,3 +5,8 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Write a script that creates the MySQL server user user_0d_2
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Write a script that gives the user user_0d_2 read privilege
+
+
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
