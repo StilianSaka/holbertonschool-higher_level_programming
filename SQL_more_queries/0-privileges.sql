9756720a7d3916436168863e19428809c1fde47c
StilianSaka <stilisaka@gmail.com>
2024-10-28 09:36:37 +0000

    privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
--- Write a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
+-- List privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
-SHOW GRANT FOR 'user_0d_1' @ 'localhost';
-SHOW GRANT FOR 'user_0d_2' @ 'localhost';
+-- List privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
